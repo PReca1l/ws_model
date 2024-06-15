1cfd41a2e9aea8d4818bbbc09651f7c9d560fbee
scripthound <grinkiko@gmail.com>
2024-06-15 19:59:26 +0300

    added backend and dependencies monitoring

--- backend.py
+++ backend.py
@@ -8,8 +8,13 @@ app = FastAPI()
 @app.post("/predict")
 async def predict(file: UploadFile):
     file_bytes = file.file.read()
-    model = Inference("weights/yolov7s.pt")
+    model = Inference("weights/best.pt")
     img, report = model.run(file_bytes)
     encoded_image = base64.b64encode(img).decode('utf-8')
     return {"img": encoded_image, "report": report}
 
+
+if __name__ == "__main__":
+    import uvicorn
+
+    uvicorn.run(app, host="0.0.0.0", port=8881)
